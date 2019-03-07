75c2b25ccf0729bcd439c93848ec9d4cd3cad844
Nguyen Luong My Ha <nguyenluongmyha@gmail.com>
2019-03-07 14:42:07 +0700

    VER1

---
+++ Google Drive API/Google Drive API/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Google_Drive_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsv_ListFile = new System.Windows.Forms.ListView();
+            this.btn_ThemFile = new System.Windows.Forms.Button();
+            this.lbl_TrangThai = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lsv_ListFile
+            // 
+            this.lsv_ListFile.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsv_ListFile.Location = new System.Drawing.Point(12, 127);
+            this.lsv_ListFile.Margin = new System.Windows.Forms.Padding(6);
+            this.lsv_ListFile.Name = "lsv_ListFile";
+            this.lsv_ListFile.Size = new System.Drawing.Size(709, 322);
+            this.lsv_ListFile.TabIndex = 0;
+            this.lsv_ListFile.UseCompatibleStateImageBehavior = false;
+            this.lsv_ListFile.View = System.Windows.Forms.View.Details;
+            // 
+            // btn_ThemFile
+            // 
+            this.btn_ThemFile.Location = new System.Drawing.Point(583, 461);
+            this.btn_ThemFile.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_ThemFile.Name = "btn_ThemFile";
+            this.btn_ThemFile.Size = new System.Drawing.Size(138, 44);
+            this.btn_ThemFile.TabIndex = 3;
+            this.btn_ThemFile.Text = "Add File";
+            this.btn_ThemFile.UseVisualStyleBackColor = true;
+            this.btn_ThemFile.Click += new System.EventHandler(this.btn_ThemFile_Click);
+            // 
+            // lbl_TrangThai
+            // 
+            this.lbl_TrangThai.AutoSize = true;
+            this.lbl_TrangThai.Location = new System.Drawing.Point(538, 86);
+            this.lbl_TrangThai.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_TrangThai.Name = "lbl_TrangThai";
+            this.lbl_TrangThai.Size = new System.Drawing.Size(62, 25);
+            this.lbl_TrangThai.TabIndex = 4;
+            this.lbl_TrangThai.Text = "Show";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(608, 84);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(6);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(113, 33);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Google_Drive_API.Properties.Resources.drive;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(104, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "16110060 - Nguyen Luong My Ha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(237, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "16110095 - Le Ba Huynh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(260, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "1614  - Nguyen Duy Phuoc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 461);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 44);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 516);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lbl_TrangThai);
+            this.Controls.Add(this.btn_ThemFile);
+            this.Controls.Add(this.lsv_ListFile);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Google Drive API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsv_ListFile;
+        private System.Windows.Forms.Button btn_ThemFile;
+        private System.Windows.Forms.Label lbl_TrangThai;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Google Drive API/Google Drive API/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace Google_Drive_API
+{
+    public partial class Form1 : Form
+    {
+        static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string ApplicationName = "Drive API .NET Quickstart";
+        private int SoFile=10;
+        UserCredential credential;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void LoadFile()
+        {
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = SoFile;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            if (files != null && files.Count > 0)
+            {
+                DataTable table = new DataTable("File");
+                table.Columns.Add(new DataColumn("FileName", typeof(string)));
+                table.Columns.Add(new DataColumn("FileID", typeof(string)));
+                foreach (var file in files)
+                {
+                    table.Rows.Add(file.Name, file.Id);
+                    //        Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+                int fc = table.Columns.Count;
+
+                lsv_ListFile.Columns.Clear();
+                lsv_ListFile.Items.Clear();
+
+                lsv_ListFile.Columns.Add("FileName");
+                lsv_ListFile.Columns.Add("FileID");
+
+                foreach (DataRow row in table.Rows)
+                {
+                    string[] subitems = new string[fc];
+
+                    object[] o = row.ItemArray;
+                    for (int i = 0; i < fc; i++)
+                    {
+                        subitems[i] = o[i].ToString();
+                    }
+                    ListViewItem item = new ListViewItem(subitems);
+                    lsv_ListFile.Items.Add(item);
+                }
+
+                lsv_ListFile.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+            else
+            {
+                MessageBox.Show("No files found.");
+            }
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            SoFile = int.Parse(numericUpDown1.Value.ToString());
+            LoadFile();
+        }
+        private void CreateFolder(string FolderName)
+        {
+            
+        }
+
+        private void btn_ThemFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
